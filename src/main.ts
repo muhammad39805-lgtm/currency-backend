6b331d8f3607d6dd480d67191299b4a5174d9d5a
NoorMuhammad140 <125381817+NoorMuhammad140@users.noreply.github.com>
2026-01-02 12:26:41 +0500

    fix: railway production start + port

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,45 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// declare const global: any;
+
+// async function bootstrap() {
+
+//   // 🛑 PREVENT DOUBLE BOOTSTRAP
+//   if (global.__app_started) {
+//     return;
+//   }
+//   global.__app_started = true;
+
+//   const app = await NestFactory.create(AppModule);
+
+//   const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+//   await app.listen(port, '0.0.0.0');
+
+//   console.log(`🚀 Server running on port ${port}`);
+// }
+
+// bootstrap();
+
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
   app.enableCors({
-    origin: '*',
+    origin: [
+      'http://localhost:4200',
+      'https://ornate-sorbet-0918f3.netlify.app',
+    ],
   });
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  const port = Number(process.env.PORT) || 3000;
 
+  await app.listen(port, '0.0.0.0');
   console.log(`🚀 Server running on port ${port}`);
 }
-bootstrap();
-
 bootstrap();

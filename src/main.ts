3cfa3d8d42828cc974fa39178a6268b6f7e30e71
NoorMuhammad140 <125381817+NoorMuhammad140@users.noreply.github.com>
2026-01-02 14:42:14 +0500

    Fix NestJS for Vercel serverless deployment

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,12 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-
-// declare const global: any;
-
-// async function bootstrap() {
-
-//   // 🛑 PREVENT DOUBLE BOOTSTRAP
-//   if (global.__app_started) {
-//     return;
-//   }
-//   global.__app_started = true;
-
-//   const app = await NestFactory.create(AppModule);
-
-//   const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-
-//   await app.listen(port, '0.0.0.0');
-
-//   console.log(`🚀 Server running on port ${port}`);
-// }
-
-// bootstrap();
-
-
-
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
 
   app.enableCors({
     origin: [
@@ -37,9 +15,9 @@ async function bootstrap() {
     ],
   });
 
-  const port = Number(process.env.PORT) || 3000;
-
-  await app.listen(port, '0.0.0.0');
-  console.log(`🚀 Server running on port ${port}`);
+  await app.init();
 }
+
 bootstrap();
+
+export default server;

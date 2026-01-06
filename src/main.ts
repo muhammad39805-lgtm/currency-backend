c4bc25aaf74d2c32995f2905cedf3f24f135b7eb
NoorMuhammad140 <125381817+NoorMuhammad140@users.noreply.github.com>
2026-01-06 16:20:41 +0500

    nmn..

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,35 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.enableCors({ origin: '*' });
+
+//   const port = process.env.PORT || 3000;
+//   await app.listen(port);
+
+//   console.log(`🚀 Server running on port ${port}`);
+// }
+
+// bootstrap();
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: '*' });
+  app.enableCors({
+    origin: '*',
+  });
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
+  // ✅ Koyeb + local both support
+  const port = process.env.PORT || 8000;
+
+  await app.listen(port, '0.0.0.0');
 
   console.log(`🚀 Server running on port ${port}`);
 }
-
 bootstrap();
